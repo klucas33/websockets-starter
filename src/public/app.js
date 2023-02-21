0a8866356185c95bdf7ac17c03f8ae1c9698a7a9
Lucas <klucas96@naver.com>
2023-02-21 14:29:05 +0900

    worked using VSCode

---
+++ src/public/app.js
@@ -0,0 +1,25 @@
+// Put all your frontend code here.
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from the Server ❌");
+});
+
+const handleSumbit = (e) => {
+  e.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+};
+
+messageForm.addEventListener("submit", handleSumbit);
